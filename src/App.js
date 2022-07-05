debe822baa5c655bb95117dc8fd772b5fda4467c
hermybairesdev <hermy.garcia@bairesdev.com>
2022-07-05 15:53:55 -0400

    adding template

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import { Button, Divider, FormControlLabel, Grid, Switch, TextField } from '@mui/material';
-import { useState } from 'react';
 import './App.css';
+import Template from './template';
 
 function App() {
-  const [custom, setCustom] = useState(true);
-  const toggleCustom = () => {
-    setCustom(!custom);
-  }
   return (
-  <Grid container p={5} rowSpacing={2}>
-    <Grid container item xs={12}>
-      <FormControlLabel control={<Switch color="primary" checked={custom} onClick={toggleCustom} />} label="Custom" labelPlacement="end"/>
-    </Grid>
-    <Grid container item xs={12}>
-      <TextField size="small" label={custom ? "Custom" : "Normal"} variant="outlined" />
-    </Grid>
-    <Grid container item xs={12}>
-      <Button variant="contained">{custom ? "Custom" : "Normal"}</Button>
-    </Grid>
-    <Grid container item xs={12}>
-      <FormControlLabel control={<Switch checked />} label={custom ? "Custom" : "Normal"} />
-    </Grid>
-  </Grid>
+  <Template />
   );
 }
 
---
+++ src/template.jsx
@@ -0,0 +1,27 @@
+import { Button, FormControlLabel, Grid, Switch, TextField } from '@mui/material';
+import { useState } from 'react';
+
+function Template() {
+  const [custom, setCustom] = useState(true);
+  const toggleCustom = () => {
+    setCustom(!custom);
+  }
+  return (
+  <Grid container p={5} rowSpacing={2}>
+    <Grid container item xs={12}>
+      <FormControlLabel control={<Switch color="primary" checked={custom} onClick={toggleCustom} />} label="Custom" labelPlacement="end"/>
+    </Grid>
+    <Grid container item xs={12}>
+      <TextField size="small" label={custom ? "Custom" : "Normal"} variant="outlined" />
+    </Grid>
+    <Grid container item xs={12}>
+      <Button variant="contained">{custom ? "Custom" : "Normal"}</Button>
+    </Grid>
+    <Grid container item xs={12}>
+      <FormControlLabel control={<Switch checked />} label={custom ? "Custom" : "Normal"} />
+    </Grid>
+  </Grid>
+  );
+}
+
+export default Template;
